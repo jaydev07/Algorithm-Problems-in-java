8c00a43179c782d85eabed26e5579f69ec8f9bef
Jaydev Bhavsar <jdbhavsar213@gmail.com>
2021-06-05 17:48:50 +0530

    Day3 ques3

--- Day3_majority_element.java
+++ Day3_majority_element.java
@@ -1,26 +1,36 @@
 import java.util.*;
 public class Day3_majority_element {
 	public int majorityElement(int[] nums) {
-        int i,maxi=1,threshold=nums.length/2;
-        
-        // First sort the elements
-        Arrays.sort(nums);
-        
-        for(i=1;i<nums.length;i++){
-        	
-        	// If the number repeats  then increment the count
-            if(nums[i] == nums[i-1]){
-                maxi+=1;
-                if(maxi>threshold){
-                    return nums[i];
-                }
-            }
-            // Else reset the count
-            else{
-                maxi=1;
-            }
-        }
-        
-        return nums[0];
-    }
+//      int i,maxi=1,threshold=nums.length/2;
+//      Arrays.sort(nums);
+//      for(i=1;i<nums.length;i++){
+//          if(nums[i] == nums[i-1]){
+//              maxi+=1;
+//              if(maxi>threshold){
+//                  return nums[i];
+//              }
+//          }else{
+//              maxi=1;
+//          }
+//      }
+     
+//      return nums[0];
+     
+     // Using moore voting algo
+     
+     int i,count=0,ele=-1;
+     
+     for(i=0;i<nums.length;i++){
+         if(count==0){
+             ele=nums[i];
+         }
+         if(ele==nums[i]){
+             count++;
+         }else{
+             count--;
+         }
+     }
+     
+     return ele;
+ }
 }
