aea0139c200300656ba683c1b46b60e899dbf48d
Jaydev Bhavsar <jdbhavsar213@gmail.com>
2021-06-20 16:07:58 +0530

    D7 Q1

---
+++ NodeNew.java
@@ -0,0 +1,12 @@
+
+public class NodeNew {
+	int val;
+    NodeNew next;
+    NodeNew random;
+
+    public NodeNew(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
